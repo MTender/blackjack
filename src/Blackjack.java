b061114e8af8513f873e5df75b5de49caa3c1d90
Märt Tender <mart.tender@ut.ee>
2021-03-06 12:48:05 +0200

    Ctrl+Alt+L

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -6,18 +6,18 @@ import java.util.Scanner;
 public class Blackjack {
 	//creating global variables and objects
 	boolean playerBlackjack, dealerBlackjack, insurance, displayConclusions = true, hasSplit;
-	int money=1000, cashAtRoundStart, totalBet, dealerFinalSum;
+	int money = 1000, cashAtRoundStart, totalBet, dealerFinalSum;
 	ArrayList<String> roundDeck, dealerCards, completeDeck = new ArrayList<>(), conclusions = new ArrayList<>();
 	ArrayList<Integer> indexOfTen = new ArrayList<>();
 	Scanner input = new Scanner(System.in);
 	Random random = new Random();
 
 
-	public boolean blackjackCheck(ArrayList<String> startingCards){
-		for(int i = 0; i<4; i++){
-			if(startingCards.contains(completeDeck.get(13 * i + 12))){
-				for(int j = 0; j<16; j++){
-					if(startingCards.contains(completeDeck.get(indexOfTen.get(j)))){
+	public boolean blackjackCheck(ArrayList<String> startingCards) {
+		for (int i = 0; i < 4; i++) {
+			if (startingCards.contains(completeDeck.get(13 * i + 12))) {
+				for (int j = 0; j < 16; j++) {
+					if (startingCards.contains(completeDeck.get(indexOfTen.get(j)))) {
 						return true;
 					}
 				}
@@ -27,74 +27,73 @@ public class Blackjack {
 	}
 
 
-	public int[] sumOfCards(ArrayList<String> cards){
+	public int[] sumOfCards(ArrayList<String> cards) {
 		//counting aces and other cards
-		int sum=0, aces=0;
-		for(String card : cards){
-			int index = completeDeck.indexOf(card)%13;
-			if(index<=8){
-				sum += (index+2);
-			}else if(index<=11){
+		int sum = 0, aces = 0;
+		for (String card : cards) {
+			int index = completeDeck.indexOf(card) % 13;
+			if (index <= 8) {
+				sum += (index + 2);
+			} else if (index <= 11) {
 				sum += 10;
-			}else aces += 1;
+			} else aces += 1;
 		}
 		return new int[]{sum, aces};
 	}
 
 
-	public void printMany(ArrayList<String> allText){
-		int lastIndex = allText.size()-1;
-		for(int i = 0; i<lastIndex; i++) System.out.print(allText.get(i)+", ");
+	public void printMany(ArrayList<String> allText) {
+		int lastIndex = allText.size() - 1;
+		for (int i = 0; i < lastIndex; i++) System.out.print(allText.get(i) + ", ");
 		System.out.println(allText.get(lastIndex));
 	}
 
 
-	public int dealer(){
+	public int dealer() {
 		//dealer actions
 		int dealerSum, dealerAces, dealerFinalSum;
-		
-		while(true){
+
+		while (true) {
 			int[] dealerSums = sumOfCards(dealerCards);
 			dealerSum = dealerSums[0];
 			dealerAces = dealerSums[1];
 
 			//choosing calculation method
-			if(dealerAces == 0){
-				if(dealerSum >= 17){
+			if (dealerAces == 0) {
+				if (dealerSum >= 17) {
 					dealerFinalSum = dealerSum;
 					break;
 				}
-			}else if(dealerSum + 10 + dealerAces >= 18 && dealerSum + 10 + dealerAces <=21){
+			} else if (dealerSum + 10 + dealerAces >= 18 && dealerSum + 10 + dealerAces <= 21) {
 				dealerFinalSum = dealerSum + 10 + dealerAces;
 				break;
-			}else if(dealerSum + dealerAces >= 17){
+			} else if (dealerSum + dealerAces >= 17) {
 				dealerFinalSum = dealerSum + dealerAces;
 				break;
 			}
 
 			//dealer next card
 			dealerCards.add(roundDeck.get(random.nextInt(roundDeck.size())));
-			roundDeck.remove(dealerCards.get(dealerCards.size()-1));
+			roundDeck.remove(dealerCards.get(dealerCards.size() - 1));
 		}
-		
+
 		return dealerFinalSum;
 	}
 
-	
-	public void begin(){
+
+	public void begin() {
 		//deck creation
 		String[] suites = new String[]{"of Hearts", "of Diamonds", "of Spades", "of Clubs"};
 		String[] pictures = new String[]{"Jack", "Queen", "King", "Ace"};
-		for(int i=0; i<4; i++){
-			for(int j=0; j<=8; j++) completeDeck.add((j+2)+" "+suites[i]);
-			for(int j=0; j<=3; j++) completeDeck.add(pictures[j]+" "+suites[i]);
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j <= 8; j++) completeDeck.add((j + 2) + " " + suites[i]);
+			for (int j = 0; j <= 3; j++) completeDeck.add(pictures[j] + " " + suites[i]);
 		}
-		ArrayList<String> tempDeck = new ArrayList<>(completeDeck); //IDE doesn't like adding ArrayList directly to itself
-		completeDeck.addAll(tempDeck); //two decks
-		
+		completeDeck.addAll(new ArrayList<>(completeDeck)); //two decks
+
 		//arraylist of indexes of cards with value 10
-		for(int j = 0; j<4; j++) for(int k = 8; k<=11; k++) indexOfTen.add(j*13+k);
-		
+		for (int j = 0; j < 4; j++) for (int k = 8; k <= 11; k++) indexOfTen.add(j * 13 + k);
+
 		//introduction and rules
 		System.out.println("""
 				-----------------------------
@@ -123,48 +122,48 @@ public class Blackjack {
 				Start new round by pressing enter.
 				Exit game by typing "exit" instead.
 				-----------------------------""");
-		
+
 		//round start
-		while(true){
+		while (true) {
 			//remaining money display
-			System.out.println("\nRemaining cash: $" +money+ "\nPress enter.");
-			
+			System.out.println("\nRemaining cash: $" + money + "\nPress enter.");
+
 			//exit conditions
-			if(money==0){
+			if (money == 0) {
 				System.out.println("Out of money. Exiting game...");
 				break;
 			}
 			String quit = input.nextLine();
-			if(quit.equals("exit")){
+			if (quit.equals("exit")) {
 				System.out.println("Exiting game...");
 				break;
 			}
-			
+
 			//bet placement
 			int bet;
 			System.out.println("Enter bet size: ");
-			while(true){
-				try{
+			while (true) {
+				try {
 					bet = input.nextInt();
-					if(bet >= 5 && bet<=300 && bet<=money){
+					if (bet >= 5 && bet <= 300 && bet <= money) {
 						break;
-					}else{
-						System.out.println("ERROR: Enter a whole number from 1 to " + (money>300?"300": money));
+					} else {
+						System.out.println("ERROR: Enter a whole number from 1 to " + (money > 300 ? "300" : money));
 					}
-				}catch(InputMismatchException ime){
+				} catch (InputMismatchException ime) {
 					System.out.println("ERROR: Enter a whole number!");
 					input.nextLine();
 				}
 			}
 			input.nextLine();
-			
+
 			//creating deck for this round
 			roundDeck = new ArrayList<>(completeDeck);
 
 			//first cards
 			ArrayList<String> playerStartingCards = new ArrayList<>(); //!!!check whether double ace split works
 			dealerCards = new ArrayList<>();
-			for(int i = 0; i<2; i++) {
+			for (int i = 0; i < 2; i++) {
 				playerStartingCards.add(roundDeck.get(random.nextInt(roundDeck.size())));
 				roundDeck.remove(playerStartingCards.get(i));
 				dealerCards.add(roundDeck.get(random.nextInt(roundDeck.size())));
@@ -178,27 +177,27 @@ public class Blackjack {
 			//display starting cards
 			System.out.println("Your cards: " + playerStartingCards.get(0) + ", " + playerStartingCards.get(1));
 			System.out.println("Dealer shows: " + dealerCards.get(0));
-			
+
 			//insurance
 			insurance = false;
-			while(money >= Math.floor(bet*1.5) && (completeDeck.indexOf(dealerCards.get(0))+1)%13==0){
+			while (money >= Math.floor(bet * 1.5) && (completeDeck.indexOf(dealerCards.get(0)) + 1) % 13 == 0) {
 				System.out.println("Would you like insurance? (y/n)");
 				String insuranceChoice = input.nextLine();
-				if(insuranceChoice.equals("y")){
+				if (insuranceChoice.equals("y")) {
 					insurance = true;
 					break;
-				}else if(insuranceChoice.equals("n")){
+				} else if (insuranceChoice.equals("n")) {
 					break;
-				}else System.out.println("ERROR: Input not recognised.");
+				} else System.out.println("ERROR: Input not recognised.");
 			}
 
 			//dealer action
-			if(!dealerBlackjack){
+			if (!dealerBlackjack) {
 				dealerFinalSum = dealer(); //playing dealer hand
-			}else{
+			} else {
 				System.out.println("Dealer shows: " + dealerCards.get(0) + ", " + dealerCards.get(1));
 			}
-			
+
 			//round start default global variables
 			totalBet = bet;
 			hasSplit = false;
@@ -208,65 +207,65 @@ public class Blackjack {
 			hand(playerStartingCards, bet);
 		}
 	}
-	
-	
-	public void hand(ArrayList<String> playerCards, int previousBet){
+
+
+	public void hand(ArrayList<String> playerCards, int previousBet) {
 		//hand start defaults
 		int playerFinalSum = 0, bet = previousBet;
 		boolean playerBust = false, hasBeenPaid = false, calcPlayerScore = false, breakNow = false;
 		boolean splitAllowed = false, doubleAllowed = false, hitAllowed = true;
-		
+
 		//game
-		if(!(playerBlackjack || dealerBlackjack)){
+		if (!(playerBlackjack || dealerBlackjack)) {
 			//starting options display
-			if(hasSplit && (completeDeck.indexOf(playerCards.get(0))+1)%13 == 0){
-				if(cashAtRoundStart >= totalBet + bet && (completeDeck.indexOf(playerCards.get(1))+1)%13 == 0){
+			if (hasSplit && (completeDeck.indexOf(playerCards.get(0)) + 1) % 13 == 0) {
+				if (cashAtRoundStart >= totalBet + bet && (completeDeck.indexOf(playerCards.get(1)) + 1) % 13 == 0) {
 					hitAllowed = false;
 					splitAllowed = true;
-					System.out.println("Dealer shows: "+dealerCards.get(0));
+					System.out.println("Dealer shows: " + dealerCards.get(0));
 					System.out.println("Stand or Split (s/sp): ");
-				}else{
+				} else {
 					calcPlayerScore = true;
 					breakNow = true;
 				}
-			}else if(cashAtRoundStart >= totalBet + bet && playerCards.size() == 2){
+			} else if (cashAtRoundStart >= totalBet + bet && playerCards.size() == 2) {
 				int index1 = completeDeck.indexOf(playerCards.get(0)), index2 = completeDeck.indexOf(playerCards.get(1));
-				if((index1 - index2) % 13 == 0 || (indexOfTen.contains(index1) && indexOfTen.contains(index2))){
+				if ((index1 - index2) % 13 == 0 || (indexOfTen.contains(index1) && indexOfTen.contains(index2))) {
 					System.out.println("Hit, Stand, Double or Split (h/s/d/sp): ");
 					splitAllowed = true;
-				}else System.out.println("Hit, Stand or Double (h/s/d): ");
+				} else System.out.println("Hit, Stand or Double (h/s/d): ");
 				doubleAllowed = true;
-			}else System.out.println("Hit or Stand (h/s): ");
-			
+			} else System.out.println("Hit or Stand (h/s): ");
+
 			//subsequent choices and calculation
-			while(true){
+			while (true) {
 				//player score calculation
-				if(calcPlayerScore){
+				if (calcPlayerScore) {
 					int[] playerSums = sumOfCards(playerCards);
 					int playerSum = playerSums[0];
 					int playerAces = playerSums[1];
 
 					//choosing calculation method
-					if(playerAces == 0 && playerSum != 21){
+					if (playerAces == 0 && playerSum != 21) {
 						playerFinalSum = playerSum;
-					}else if(playerSum + 10 + playerAces<21){
+					} else if (playerSum + 10 + playerAces < 21) {
 						playerFinalSum = playerSum + 10 + playerAces;
-					}else if(playerSum + 10 + playerAces == 21 || playerSum + playerAces == 21){
+					} else if (playerSum + 10 + playerAces == 21 || playerSum + playerAces == 21) {
 						System.out.println("21");
 						playerFinalSum = 21;
 						breakNow = true;
-					}else playerFinalSum = playerSum + playerAces;
-					
+					} else playerFinalSum = playerSum + playerAces;
+
 					//player bust control
-					if(playerSum + playerAces>21){
+					if (playerSum + playerAces > 21) {
 						if (hasSplit) System.out.println("Player bust.");
 						playerBust = true;
 						breakNow = true;
 					}
 					//end hand
-					if(breakNow){
+					if (breakNow) {
 						//display dealer cards
-						if(displayConclusions && !hasSplit){
+						if (displayConclusions && !hasSplit) {
 							System.out.print("Dealer shows: ");
 							printMany(dealerCards);
 							displayConclusions = false;
@@ -277,10 +276,10 @@ public class Blackjack {
 					System.out.println("Dealer shows: " + dealerCards.get(0));
 					System.out.println("Hit or Stand (h/s): ");
 				}
-				
+
 				//player actions
 				String playChoice = input.nextLine();
-				if(playChoice.equals("h") && hitAllowed){
+				if (playChoice.equals("h") && hitAllowed) {
 					//action hit
 					calcPlayerScore = true;
 					playerCards.add(roundDeck.get(random.nextInt(roundDeck.size())));
@@ -288,11 +287,11 @@ public class Blackjack {
 					System.out.print("Your cards: ");
 					printMany(playerCards);
 					splitAllowed = false;
-				}else if(playChoice.equals("s")){
+				} else if (playChoice.equals("s")) {
 					//action stand
 					calcPlayerScore = true;
 					breakNow = true;
-				}else if(playChoice.equals("d")  && doubleAllowed){
+				} else if (playChoice.equals("d") && doubleAllowed) {
 					//action double down
 					calcPlayerScore = true;
 					bet *= 2;
@@ -302,7 +301,7 @@ public class Blackjack {
 					roundDeck.remove(playerCards.get(2));
 					System.out.println("Your cards: " + playerCards.get(0) + ", " + playerCards.get(1) + ", " + playerCards.get(2));
 					breakNow = true;
-				}else if(playChoice.equals("sp") && splitAllowed){
+				} else if (playChoice.equals("sp") && splitAllowed) {
 					//action split
 					hasSplit = true;
 					totalBet += bet;
@@ -314,74 +313,74 @@ public class Blackjack {
 					roundDeck.remove(playerCards.get(1));
 					playerCards2.add(roundDeck.get(random.nextInt(roundDeck.size())));
 					roundDeck.remove(playerCards2.get(1));
-					System.out.println("Playing first hand: " + playerCards.get(0) +", "+ playerCards.get(1));
+					System.out.println("Playing first hand: " + playerCards.get(0) + ", " + playerCards.get(1));
 					hand(playerCards, bet);
-					System.out.println("Playing second hand: " + playerCards2.get(0) +", "+ playerCards2.get(1));
+					System.out.println("Playing second hand: " + playerCards2.get(0) + ", " + playerCards2.get(1));
 					hand(playerCards2, bet);
 					hasBeenPaid = true;
 					//display dealer cards and conclusions
-					if(displayConclusions){
+					if (displayConclusions) {
 						System.out.print("Dealer shows: ");
 						printMany(dealerCards);
 						printMany(conclusions);
 						displayConclusions = false;
 					}
 					break;
-				}else{
+				} else {
 					//unrecognised input
 					calcPlayerScore = false;
 					System.out.println("ERROR: Input not recognised.");
 				}
 			}
 			//no splits or done with splits
-			if(!hasBeenPaid){
+			if (!hasBeenPaid) {
 				//payout
-				if(!playerBust){
+				if (!playerBust) {
 					String message = payout(dealerFinalSum, playerFinalSum, bet);
-					if(!message.equals("Dealer bust") || !conclusions.contains("Dealer bust")){
+					if (!message.equals("Dealer bust") || !conclusions.contains("Dealer bust")) {
 						conclusions.add(message);
 					}
-				}else{
+				} else {
 					money -= bet;
 					conclusions.add("Player bust");
 				}
 				//display conclusion
-				if(!hasSplit) System.out.println(conclusions.get(0));
+				if (!hasSplit) System.out.println(conclusions.get(0));
 			}
-		}else System.out.println(payout(0, 0, bet)); //payout if dealer or player has blackjack
+		} else System.out.println(payout(0, 0, bet)); //payout if dealer or player has blackjack
 	}
 
-	
-	public String payout(int dealerFinalSum, int playerFinalSum, int bet){
+
+	public String payout(int dealerFinalSum, int playerFinalSum, int bet) {
 		//insurance loss check
-		if(insurance && !dealerBlackjack){
-			money -= Math.floor(bet*0.5);
+		if (insurance && !dealerBlackjack) {
+			money -= Math.floor(bet * 0.5);
 		}
 		//finding winner and paying
-		if(dealerFinalSum>21){
-			money +=bet;
+		if (dealerFinalSum > 21) {
+			money += bet;
 			return "Dealer bust";
-		}else if(playerFinalSum>dealerFinalSum){
-			money +=bet;
+		} else if (playerFinalSum > dealerFinalSum) {
+			money += bet;
 			return "Player wins";
-		}else if(playerFinalSum<dealerFinalSum || (dealerBlackjack && !playerBlackjack)){
-			if(insurance && dealerBlackjack){
-				money -=bet;
-				money +=(2*Math.floor(bet*0.5));
-				return "Dealer blackjack. Insurance pays";
-			}else{
+		} else if (playerFinalSum < dealerFinalSum || (dealerBlackjack && !playerBlackjack)) {
+			if (insurance && dealerBlackjack) {
 				money -= bet;
-				if(dealerBlackjack) return "Dealer blackjack";
+				money += (2 * Math.floor(bet * 0.5));
+				return "Dealer blackjack. Insurance pays";
+			} else {
+				money -= bet;
+				if (dealerBlackjack) return "Dealer blackjack";
 				return "Dealer wins";
 			}
-		}else if(playerBlackjack && !dealerBlackjack){
-			money +=Math.floor(bet*1.5);
+		} else if (playerBlackjack && !dealerBlackjack) {
+			money += Math.floor(bet * 1.5);
 			return "Blackjack!";
-		}else return "Push";
+		} else return "Push";
 	}
 
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Blackjack game = new Blackjack();
 		game.begin();
 	}
